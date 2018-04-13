8e91186b5fe7d2e09d3e5d87a575ba0a95738cb7
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2018-04-13 14:09:59 +0100

    Add files via upload

---
+++ template.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @file
+ * The primary PHP file for this theme.
+ */
+
+ 
+ 
+function aes_international_preprocess_page(&$vars) { 
+    // template files called page--contenttype.tpl.php 
+    if (isset($vars['node']->type)) { 
+        $vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->type; 
+    }
+	
+	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/logoscroll.js');
+	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/fade-text.js');
+}
+
+function aes_international_theme(){
+	$items=array();
+	
+	$items['user_login']=array(
+	'render element' => 'form',
+	'path'=> drupal_get_path('theme', 'aesbs337').'/templates',
+	'template'=>'user_login',
+	'preprocess functions'=>array(
+	'aesbs337_preprocess_user_login'
+	),
+	);
+	return $items;
+}
+
+function aes_international_preprocess_node(&$variables) {
+  $node = $variables['node'];
+  $date = format_date($node->created, 'custom', 'F j, Y');
+  $variables['submitted'] = t('Submitted by !username on !datetime', array('!username' => $variables['name'], '!datetime' => $date));
+}
