364288d73d7d96e2c3f50e17408b47c83b0a3fd6
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2018-08-01 11:18:55 +0100

    Updating the js file call to be minified instead of normal js

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -77,7 +77,7 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/brands.css" integrity="sha384-VGCZwiSnlHXYDojsRqeMn3IVvdzTx5JEuHgqZ3bYLCLUBV8rvihHApoA1Aso2TZA" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/fontawesome.css" integrity="sha384-GVa9GOgVQgOk+TNYXu7S/InPTfSDTtBalSgkgqQ7sCik56N9ztlkoTr2f/T44oKV" crossorigin="anonymous">
 <link rel="stylesheet" href="/sites/all/libraries/flexslider/flexslider.css" type="text/css">
-<script src="/sites/all/libraries/flexslider/jquery.flexslider.js"></script>
+<script src="/sites/all/libraries/flexslider/jquery.flexslider-min.js"></script>
 <script type="text/javascript" charset="utf-8">
 (function($){
 	$(window).load(function() {
