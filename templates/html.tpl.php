349468c14c03fb5fdc94f787d0ffc6edd75bc996
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-03-26 13:57:52 +0000

    Cleaned up the script testsed seems to read fine with no errors

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -67,17 +67,17 @@
     <script src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv-printshiv.min.js"></script>
   <![endif]-->
   <?php
-  if (preg_match('~MSIE|Internet Explorer~i', $_SERVER['HTTP_USER_AGENT']) || (strpos($_SERVER['HTTP_USER_AGENT'], 'Trident/7.0; rv:11.0') !== false)) {
-  $origin =$_SERVER['HTTP_ORIGIN'];
-  $allowed_domains= [
-    'https://js.hsleadflows.net',
-    'https://js.usemessages.com',
-  ];
+    if (preg_match('~MSIE|Internet Explorer~i', $_SERVER['HTTP_USER_AGENT']) || (strpos($_SERVER['HTTP_USER_AGENT'], 'Trident/7.0; rv:11.0') !== false)) {
+    $origin =$_SERVER['HTTP_ORIGIN'];
+    $allowed_domains= [
+      'https://js.hsleadflows.net',
+      'https://js.usemessages.com',
+    ];
 
-  if(in_array, $allowed_domains)){
-    header('Access-Control-Allow-Origin: ' . $origin);
+    if(in_array($origin, $allowed_domains)){
+      header('Access-Control-Allow-Origin: ' . $origin);
+    }
   }
-}
   ?>
 
   <?php print $scripts; ?>
