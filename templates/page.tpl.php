e83bb27548228777c3f327505b1e413e71a84067
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-10-04 15:50:04 +0100

    New js in page.tpl.php to give  divs to the flexslider for new classes

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -86,10 +86,13 @@
 		});
 
 	$.each($(".flexslider .slides li img"), function() {
+		var newContainer = document.createElement("div");
     var newDiv = document.createElement("div");
 	var titleDiv = document.createElement("div");
     var titleText = $(this).attr('title');
 	var altText = $(this).attr('alt')
+	$(newContainer).html(titleDiv, newDiv);
+	$(newContainer).attr('class', 'caption-container flex-item-caption');
     $(titleDiv).html(titleText);
     $(titleDiv).attr('class','flex-header');
 	$(newDiv).html(altText);
