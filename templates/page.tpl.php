961128b131f8046c43b7664118e8f4540dd89138
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-10-07 10:53:29 +0100

    correctly firing script plus placement

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -85,21 +85,24 @@
 
 		});
 
-	$.each($(".flexslider .slides li img"), function() {
-		var newContainer = document.createElement("div");
-    var newDiv = document.createElement("div");
-	var titleDiv = document.createElement("div");
-    var titleText = $(this).attr('title');
-	var altText = $(this).attr('alt');
-	$(newContainer).html(titleDiv, newDiv);
-	$(newContainer).attr('class', 'caption-container flex-item-caption');
-    $(titleDiv).html(titleText);
-    $(titleDiv).attr('class','flex-header');
-	$(newDiv).html(altText);
-	$(newDiv).attr('class','flex-caption');
-    $(this).after(newDiv);
-	$(this).after(titleDiv);
-	})
+		$.each($(".flexslider .slides li img"), function() {
+			var newContainer = document.createElement("div");
+	    var newDiv = document.createElement("div");
+		var titleDiv = document.createElement("div");
+	    var titleText = $(this).attr('title');
+		var altText = $(this).attr('alt');
+		$(newContainer).html(titleDiv, newDiv);
+		$(newContainer).attr('class', 'caption-container flex-item-caption');
+	    $(titleDiv).html(titleText);
+	    $(titleDiv).attr('class','flex-header');
+		$(newDiv).html(altText);
+		$(newDiv).attr('class','flex-caption');
+		$(this).after(newContainer);
+	    $(this).after(newDiv);
+		$(this).after(titleDiv);
+		$(".caption-container").append(titleDiv, newDiv);
+
+		})
 
 	})
 })(jQuery);
