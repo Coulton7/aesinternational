284ded5dcd185d64535c6540aa19dc60977ceef7
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-10-07 10:40:21 +0100

    New script attempt to add the classes

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -91,13 +91,12 @@
 		var titleDiv = document.createElement("div");
 	    var titleText = $(this).attr('title');
 		var altText = $(this).attr('alt');
-		$(newContainer).html(titleDiv, newDiv);
 		$(newContainer).attr('class', 'caption-container flex-item-caption');
 	    $(titleDiv).html(titleText);
 	    $(titleDiv).attr('class','flex-header');
 		$(newDiv).html(altText);
 		$(newDiv).attr('class','flex-caption');
-		$(this).after(newContainer);
+		$(newDiv, titleDiv).after(newContainer);
 	    $(this).after(newDiv);
 		$(this).after(titleDiv);
 		})
