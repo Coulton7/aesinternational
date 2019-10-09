e96567772f464a810061053129eebed83cb7bbbf
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-10-09 10:56:11 +0100

    tidying the tpl up some more

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -88,20 +88,19 @@
 		$.each($(".flexslider .slides li img"), function() {
 			var newContainer = document.createElement("div");
 	    var newDiv = document.createElement("div");
-		var titleDiv = document.createElement("div");
+			var titleDiv = document.createElement("div");
 	    var titleText = $(this).attr('title');
-		var altText = $(this).attr('alt');
-		$(newContainer).html(titleDiv, newDiv);
-		$(newContainer).attr('class', 'caption-container flex-item-caption');
+			var altText = $(this).attr('alt');
+			$(newContainer).html(titleDiv, newDiv);
+			$(newContainer).attr('class', 'caption-container flex-item-caption');
 	    $(titleDiv).html(titleText);
 	    $(titleDiv).attr('class','flex-header');
-		$(newDiv).html(altText);
-		$(newDiv).attr('class','flex-caption');
-		$(this).after(newContainer);
+			$(newDiv).html(altText);
+			$(newDiv).attr('class','flex-caption');
+			$(this).after(newContainer);
 	    $(this).after(newDiv);
-		$(this).after(titleDiv);
-		$(".caption-container").append(titleDiv, newDiv);
-
+			$(this).after(titleDiv);
+			$(".caption-container").append(titleDiv, newDiv);
 		})
 
 	})
