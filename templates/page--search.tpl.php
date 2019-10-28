06362d458bfaa130d13dd4e2a407ecd195902865
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-10-28 09:15:12 +0000

    Restructuring the socialmedia area to look like com and css to match

--- templates/page--search.tpl.php
+++ templates/page--search.tpl.php
@@ -193,12 +193,12 @@
 
 		<?php if ($page['wavelower']): ?>
 		<div class="row wavelower row-eq-height">
-			<div class="col-sm-9 col-xs-1 fullscreen">
+			<div class="col-sm-9 col-xs-0 fullscreen">
 				<div class="bottom-gap">
 				</div>
 			</div>
 
-			<div class="col-sm-3 col-xs-10 fullscreen">
+			<div class="col-sm-3 col-xs-12 fullscreen">
 
 				<img class="curve-down" src = "/sites/all/themes/aesbs337/images/logos/wave-lower.svg" alt ="curve-down"></img>
 
