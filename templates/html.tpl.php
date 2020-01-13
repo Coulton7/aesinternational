a4c72f687d4c7534c81027d228be2a3202a3c028
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-01-13 12:10:39 +0000

    Removed the script type call to try and remove the 404 error on the call

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -82,7 +82,7 @@
   ?>
 
   <?php print $scripts; ?>
-<script type="text/javascript" src="/sites/all/themes/aesbs337/js/chosen-init.js"></script>
+<script src="/sites/all/themes/aesbs337/js/chosen-init.js"></script>
 </head>
 <body<?php print $body_attributes; ?>>
   <div id="skip-link">
