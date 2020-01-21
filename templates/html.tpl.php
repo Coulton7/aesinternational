dc26da9b6c5dbe51f1750d2bcb9dcfd8f8d3f52d
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-01-21 14:52:03 +0000

    updating html.tpl.php to reove inline script

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -68,20 +68,7 @@
     <script src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv-printshiv.min.js"></script>
   <![endif]-->
   <?php print $scripts; ?>
-
-  <script type="text/javascript">
-
-  (function ($){
-  $( document ).ready(function() {
-    $(".form-select").chosen({
-      disable_search_threshold: 10,
-      no_results_text: "Sorry, no results found."
-    })
-  });
-  })(jQuery);
-
-
-</script>
+<script src="/sites/all/themes/aesbs337/js/chosen-init.js"></script>
 </head>
 <body<?php print $body_attributes; ?>>
   <div id="skip-link">
