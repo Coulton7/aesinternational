ad470ff65b387e2b00050e65cfa5a0b947446294
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-10 11:29:53 +0000

    REmoved php region print command

--- templates/maintenance-page.tpl.php
+++ templates/maintenance-page.tpl.php
@@ -88,7 +88,6 @@
 
  				<img class="curve-down" src = "/sites/all/themes/aesbs337/images/logos/wave-lower.svg" alt ="curve-down"></img>
 
- 				<?php print render($page['wavelower']); ?>
  				<div class="socialmedia text-center">
 
  				</div>
