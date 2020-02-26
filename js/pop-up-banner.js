cd857f0bee7f63f067ee5a184fa0bd56b695f631
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-26 13:53:03 +0000

    Updated the pop-up-banner.js so it has corrct timeout method

--- js/pop-up-banner.js
+++ js/pop-up-banner.js
@@ -1,32 +1,39 @@
-(function($) {
-  vex.defaultOptions.className = 'vex-theme-top';
- $(document).ready(function(){
-   if(sessionStorage.getItem('vexClosed') === 'closed')
-   {
-     return null;
-   }
-   else
-   {
-       vex.dialog.open({
-         message: "Can't find what you are looking for maybe try going to aesseal.com",
-         contentClassName: 'web-banner text-center',
-         buttons: [
-           $.extend({}, vex.dialog.buttons.YES, {
-             className: 'col-lg-4 col-lg-pull-2 col-xs-5 col-xs-pull-1 btn-red vex-ext',
-             text: 'Go to aesseal.com',
-             click: function() {
-               window.open('https://www.aesseal.com/','_blank');
-             }
-           }),
-           $.extend({}, vex.dialog.buttons.NO, {
-             className: 'col-xs-5 col-xs-pull-1 col-lg-4 col-lg-pull-2 btn-midgrey vex-closes',
-             text: 'Remain on this site',
-           })
-         ],
-         afterClose: function() {
-           sessionStorage.setItem('vexClosed','closed');
-         }
-       });
-     }
-   });
- })(jQuery);
+vex.defaultOptions.className = 'vex-theme-top';
+
+  var total_seconds = window.sessionStorage.getItem('total_seconds');
+
+  total_seconds = (total_seconds != null && total_seconds > 0) ? total_seconds : 60 * 5;
+
+  function CheckTime() {
+    if (total_seconds <= 0) {
+      if (sessionStorage.getItem('vexClosed') != 'closed') {
+        vex.dialog.open({
+          message: "Can't find what you are looking for maybe try going to aesseal.com",
+          contentClassName: 'web-banner text-center',
+          buttons: [
+            Object.assign({}, vex.dialog.buttons.YES, {
+              className: 'col-lg-4 col-lg-pull-2 col-xs-5 col-xs-pull-1 btn-red',
+              text: 'Go to aesseal.com',
+              click: function() {
+                window.location.href = 'https://www.aesseal.com/';
+              }
+            }),
+            Object.assign({}, vex.dialog.buttons.NO, {
+              className: 'col-xs-5 col-xs-pull-1 col-lg-4 col-lg-pull-2 btn-default',
+              text: 'Remain on this site',
+            })
+          ],
+          afterClose: function() {
+            sessionStorage.setItem('vexClosed', 'closed');
+          }
+        });
+      }
+    } else {
+      total_seconds = total_seconds - 1;
+
+      setTimeout("CheckTime()", 1000);
+    }
+
+    window.sessionStorage.setItem('total_seconds', total_seconds);
+  }
+  setTimeout("CheckTime()", 1000);
