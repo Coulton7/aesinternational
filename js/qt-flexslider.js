e6583cf7c8e43f1462a32d32e33dd0c23f7924cc
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-09-22 08:51:51 +0100

    changing the call for the ks to pick up click to see if it has an effect

--- js/qt-flexslider.js
+++ js/qt-flexslider.js
@@ -1,6 +1,6 @@
 (function($){
   $(document).ready(function() {
-    $('#quicktabs-your_regional_branch .scrtabs-tab-container .scrtabs-tabs-fixed-container .scrtabs-tabs-movable-container .nav-tabs').children('li').find('a').on('click', function(){
+    $('#quicktabs-your_regional_branch .scrtabs-tab-container .scrtabs-tabs-fixed-container .scrtabs-tabs-movable-container .nav-tabs li a').on('click', function(){
       $('.flexslider').resize();
     });
   });
