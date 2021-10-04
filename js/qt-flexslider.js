fe246755a196a6d171878eb0288d8b0156dfd987
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-10-04 12:46:20 +0100

    Adding a console log element to see if Got the right command working

--- js/qt-flexslider.js
+++ js/qt-flexslider.js
@@ -2,6 +2,7 @@
   $(document).ready(function() {
     $('#quicktabs-your_regional_branch .scrtabs-tab-container .scrtabs-tabs-fixed-container .scrtabs-tabs-movable-container .nav-tabs li a').on('click', function(){
       $('.flexslider').resize();
+      console.log('Tab changed');
     });
   });
 })(jQuery);
