eef0d1ff623463c389647abf4926238a181ec779
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-11-24 11:59:23 +0000

    Updating the js file so the script will work correctly

--- js/menu-collapse.js
+++ js/menu-collapse.js
@@ -1,9 +1,11 @@
-function autocollapse() {
-  var navbar = $('#autocollapse');
-  navbar.removeClass('collapsed'); // set standart view
-  if(navbar.innerHeight() > 60) // check if we've got 2 lines
+(function($){
+  function autocollapse() {
+    var navbar = $('#autocollapse');
+    navbar.removeClass('collapsed'); // set standart view
+    if(navbar.innerHeight() > 60) // check if we've got 2 lines
     navbar.addClass('collapsed'); // force collapse mode
-}
+  }
 
-$(document).on('ready', autocollapse);
-$(window).on('resize', autocollapse);
+  $(document).on('ready', autocollapse);
+  $(window).on('resize', autocollapse);
+})(jQuery);
