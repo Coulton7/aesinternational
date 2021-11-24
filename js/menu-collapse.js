47ea9cf39d14100adeffd6b2df4f21b1560a8d1f
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-11-24 12:35:03 +0000

    Adding extra add class element need to test if removes it

--- js/menu-collapse.js
+++ js/menu-collapse.js
@@ -4,6 +4,7 @@
     navbar.removeClass('collapsed'); // set standart view
     if(navbar.innerHeight() > 60) // check if we've got 2 lines
     navbar.addClass('collapsed'); // force collapse mode
+    document.getElementById('overlay-btn').addClass('display-none');
   }
 
   $(document).on('ready', autocollapse);
