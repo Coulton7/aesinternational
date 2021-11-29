3da5bc59a4f2eff8be71eb9771e9ceeea1e2f089
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-11-29 12:29:35 +0000

    Adding the new js to hide btns when the overlay each other

--- js/menu-collapse.js
+++ js/menu-collapse.js
@@ -11,6 +11,30 @@
     }
   }
 
+  function menuOverlay() {
+    var overlayBtn = document.querySelector('.overlay-btn').getBoundingClientRect();
+    var overlayBtnLeft = overlayBtn.left;
+    var overlayBtnRight = overlayBtn.right
+
+    var menuItem = document.querySelector('.last').getBoundingClientRect();
+    var menuItemLeft = menuItem.left;
+    var menuItemRight = menuItem.right
+
+    if ((menuItemRight > overlayBtnLeft && menuItemRight < overlayBtnRight) || (menuItemLeft < overlayBtnRight && menuItemLeft > overlayBtnLeft)) {
+      var horizontalMatch = true
+    } else {
+      var horizontalMatch = false
+    }
+
+    if (horizontalMatch) {
+      $('#overlay-btn').addClass('display-none')
+    } else {
+      $('#overlay-btn').removeClass('display-none')
+    }
+  }
+
   $(document).on('ready', autocollapse);
+  $(document).on('ready', menuOverlay);
   $(window).on('resize', autocollapse);
+  $(window).on('resize', menuOverlay);
 })(jQuery);
