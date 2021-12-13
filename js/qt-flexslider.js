a790c85c37ea30a55a3430f8e7f1179498637cd4
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-12-13 16:08:55 +0000

    Adding new jquery variation for quicktabs for testing on int sites

--- js/qt-flexslider.js
+++ js/qt-flexslider.js
@@ -1,6 +1,34 @@
 (function($){
   $(document).ready(function() {
+    $('#quicktabs-your_regional_branch1').on('click', function(){
+      $('.flexslider').resize();
+      console.log('Tab changed');
+    });
+    $('#quicktabs-your_regional_branch2').on('click', function(){
+      $('.flexslider').resize();
+      console.log('Tab changed');
+    });
+    $('#quicktabs-your_regional_branch3').on('click', function(){
+      $('.flexslider').resize();
+      console.log('Tab changed');
+    });
+    $('#quicktabs-your_regional_branch4').on('click', function(){
+      $('.flexslider').resize();
+      console.log('Tab changed');
+    });
+    $('#quicktabs-your_regional_branch5').on('click', function(){
+      $('.flexslider').resize();
+      console.log('Tab changed');
+    });
     $('#quicktabs-your_regional_branch6').on('click', function(){
+      $('.flexslider').resize();
+      console.log('Tab changed');
+    });
+    $('#quicktabs-your_regional_branch7').on('click', function(){
+      $('.flexslider').resize();
+      console.log('Tab changed');
+    });
+    $('#quicktabs-your_regional_branch8').on('click', function(){
       $('.flexslider').resize();
       console.log('Tab changed');
     });
