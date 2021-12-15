8c103f696806fe61f064e317d6a316bd4a0bdf8e
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-12-15 12:18:55 +0000

    Reset back to previous working on tab content not actual tabs

--- js/qt-flexslider.js
+++ js/qt-flexslider.js
@@ -1,34 +1,34 @@
 (function($){
   $(document).ready(function() {
-    $('a#quicktabs-your_regional_branch1').on('click', function(){
+    $('#quicktabs-your_regional_branch1').on('click', function(){
       $('.flexslider').resize();
       console.log('Tab changed');
     });
-    $('a#quicktabs-your_regional_branch2').on('click', function(){
+    $('#quicktabs-your_regional_branch2').on('click', function(){
       $('.flexslider').resize();
       console.log('Tab changed');
     });
-    $('a#quicktabs-your_regional_branch3').on('click', function(){
+    $('#quicktabs-your_regional_branch3').on('click', function(){
       $('.flexslider').resize();
       console.log('Tab changed');
     });
-    $('a#quicktabs-your_regional_branch4').on('click', function(){
+    $('#quicktabs-your_regional_branch4').on('click', function(){
       $('.flexslider').resize();
       console.log('Tab changed');
     });
-    $('a#quicktabs-your_regional_branch5').on('click', function(){
+    $('#quicktabs-your_regional_branch5').on('click', function(){
       $('.flexslider').resize();
       console.log('Tab changed');
     });
-    $('a#quicktabs-your_regional_branch6').on('click', function(){
+    $('#quicktabs-your_regional_branch6').on('click', function(){
       $('.flexslider').resize();
       console.log('Tab changed');
     });
-    $('a#quicktabs-your_regional_branch7').on('click', function(){
+    $('#quicktabs-your_regional_branch7').on('click', function(){
       $('.flexslider').resize();
       console.log('Tab changed');
     });
-    $('a#quicktabs-your_regional_branch8').on('click', function(){
+    $('#quicktabs-your_regional_branch8').on('click', function(){
       $('.flexslider').resize();
       console.log('Tab changed');
     });
