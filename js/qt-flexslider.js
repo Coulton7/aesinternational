1616d0dec5bf9318853c0183d1281e686cf0980b
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-12-15 13:57:05 +0000

    Removing th console log as I've new seen it is firing the js

--- js/qt-flexslider.js
+++ js/qt-flexslider.js
@@ -2,35 +2,27 @@
   $(document).ready(function() {
     $('#quicktabs-your_regional_branch1').on('mouseenter', function(){
       $('.flexslider').resize();
-      console.log('Tab changed');
     });
     $('#quicktabs-your_regional_branch2').on('mouseenter', function(){
       $('.flexslider').resize();
-      console.log('Tab changed');
     });
     $('#quicktabs-your_regional_branch3').on('mouseenter', function(){
       $('.flexslider').resize();
-      console.log('Tab changed');
     });
     $('#quicktabs-your_regional_branch4').on('mouseenter', function(){
       $('.flexslider').resize();
-      console.log('Tab changed');
     });
     $('#quicktabs-your_regional_branch5').on('mouseenter', function(){
       $('.flexslider').resize();
-      console.log('Tab changed');
     });
     $('#quicktabs-your_regional_branch6').on('mouseenter', function(){
       $('.flexslider').resize();
-      console.log('Tab changed');
     });
     $('#quicktabs-your_regional_branch7').on('mouseenter', function(){
       $('.flexslider').resize();
-      console.log('Tab changed');
     });
     $('#quicktabs-your_regional_branch8').on('mouseenter', function(){
       $('.flexslider').resize();
-      console.log('Tab changed');
     });
   });
 })(jQuery);
