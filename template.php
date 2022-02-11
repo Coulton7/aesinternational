65e46edf3e888f02d0a62f0bc4de7bbfffa31b42
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-02-11 14:33:46 +0000

    Re-adding the contact icon js to the site

--- template.php
+++ template.php
@@ -11,6 +11,7 @@ function aesinternational_preprocess_page(&$vars) {
   drupal_add_library('system','ui.dialog');
   drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/logoscroll.js');
   drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/fade-text.js');
+  drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/contact-icon.js');
   drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/nav-btn.js');
   drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/slider-options.js');
   drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/jquery.scrolling-tabs.min.js');
@@ -20,7 +21,6 @@ function aesinternational_preprocess_page(&$vars) {
 
 function aesinternational_js_alter(&$javascript){
   unset($javascript[drupal_get_path('theme', 'aesbs337').'/js/reg-mark.js']);
-  unset($javascript[drupal_get_path('theme', 'aesbs337').'/js/contact-icon.js']);
 }
 
 function aesinternational_css_alter(&$css){
